41a3d885ebab4657af80fd45f656b73e85ba2ddc
wim07101993 <van.laer.wim@gmail.com>
2019-02-17 14:03:45 +0100

    Added wpf

---
+++ Cranium.WPF/App.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+using Cranium.WPF.Views;
+
+namespace Cranium.WPF
+{
+    public partial class App
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            MainWindow = new MainWindow();
+            MainWindow.Show();
+        }
+    }
+}
---
+++ Cranium.WPF/Views/MainWindow.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Cranium.WPF.Views
+{
+    public partial class MainWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
