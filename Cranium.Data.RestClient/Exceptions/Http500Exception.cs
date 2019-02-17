a02358868d64f5b62de586c80d72c9641bd14b7f
wim07101993 <van.laer.wim@gmail.com>
2019-02-17 13:53:14 +0100

    implemented client methods + removed UWP

---
+++ Cranium.Data.RestClient/Exceptions/Http500Exception.cs
@@ -0,0 +1,20 @@
+﻿using System.Net;
+using System.Net.Http;
+
+namespace Cranium.Data.RestClient.Exceptions
+{
+    public class Http500Exception : HttpException
+    {
+        public Http500Exception(string message) : base(message)
+        {
+        }
+
+        public Http500Exception(HttpStatusCode statusCode, string message) : base(statusCode, message)
+        {
+        }
+
+        public Http500Exception(HttpResponseMessage response) : base(response)
+        {
+        }
+    }
+}
---
+++ Shared/Extensions/StringExtensions.cs
@@ -0,0 +1,14 @@
+﻿using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace Shared.Extensions
+{
+    public static class StringExtensions
+    {
+        public static T DeserializeJson<T>(this string s)
+            => JsonConvert.DeserializeObject<T>(s);
+
+        public static async Task<T> DeserializeJsonAsync<T>(this Task<string> s)
+            => JsonConvert.DeserializeObject<T>(await s);
+    }
+}
