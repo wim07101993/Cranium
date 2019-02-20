84c694bbc238ccd0f7bc5c1efe27b92252311d86
wim07101993 <van.laer.wim@gmail.com>
2019-02-20 21:42:38 +0100

    beginning of answer columnt

---
+++ Cranium.WPF/Views/Controls/AnswerDatagridColumn.cs
@@ -0,0 +1,18 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace Cranium.WPF.Views.Controls
+{
+    public class AnswerDatagridColumn : DataGridBoundColumn
+    {
+        protected override FrameworkElement GenerateElement(DataGridCell cell, object dataItem)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        protected override FrameworkElement GenerateEditingElement(DataGridCell cell, object dataItem)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ Cranium.WPF/Views/Controls/AnswerEditingControl.cs
@@ -0,0 +1,21 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using Cranium.Data.RestClient.Models;
+
+namespace Cranium.WPF.Views.Controls
+{
+    public class AnswerEditingControl : Control
+    {
+        public static readonly DependencyProperty AnswerProperty = DependencyProperty.Register(
+            nameof(Answer),
+            typeof(Answer),
+            typeof(AnswerEditingControl),
+            new PropertyMetadata(default(Answer)));
+
+        public Answer Answer
+        {
+            get => (Answer) GetValue(AnswerProperty);
+            set => SetValue(AnswerProperty, value);
+        }
+    }
+}
