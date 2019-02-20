6587cdd00eac6b345b52c135c30e9b74b1ff152a
wim van laer <van.laer.wim@gmail.com>
2019-02-20 13:19:08 +0100

    working on tiles for gameboard

---
+++ Cranium.Data/Models/Tile.cs
@@ -0,0 +1,17 @@
+﻿using Cranium.Data.Models.Bases;
+using System;
+using System.Collections.Generic;
+
+namespace Cranium.Data.Models
+{
+    public class Tile : AWithId
+    {
+        public Guid CategoryId { get; set; }
+        public bool IsShortCut { get; set; }
+
+        public ICollection<Guid> NextTiles { get; set; }
+
+        public bool IsEnd { get; set; }
+        public bool IsStart { get; set; }
+    }
+}
