bf4f89a4bf322e631cfab583791901799a9a4c1e
wim van laer <van.laer.wim@gmail.com>
2019-02-21 13:22:09 +0100

    beginning of the gameboard service

---
+++ Cranium.Data/Services/IGameBoardService.cs
@@ -0,0 +1,12 @@
+﻿using Cranium.Data.Models;
+using System;
+using System.Threading.Tasks;
+
+namespace Cranium.Data.Services
+{
+    public interface IGameBoardService
+    {
+        Task<Tiles> GenerateTilesAsync(int countOfCycles);
+        Task<Tiles> GenerateTilesAsync(TimeSpan gameTime);
+    }
+}
