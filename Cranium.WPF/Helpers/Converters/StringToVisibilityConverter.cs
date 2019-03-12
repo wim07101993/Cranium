b2bbd47976bc97fc24ffa0d2361c9562e1d433f8
wim van laer <van.laer.wim@gmail.com>
2019-03-12 12:46:36 +0100

    Created string to visibility converter

---
+++ Cranium.WPF/Helpers/Converters/StringToVisibilityConverter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace Cranium.WPF.Helpers.Converters
+{
+    public class StringToVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            switch (value)
+            {
+                case string s:
+                    return string.IsNullOrWhiteSpace(s)
+                         ? Visibility.Collapsed
+                         : Visibility.Visible;
+                default:
+                    return Visibility.Collapsed;
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
