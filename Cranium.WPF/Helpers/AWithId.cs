f760a7ad23d2eb4bee8eb2540dd4750bfa720965
wim van laer <van.laer.wim@gmail.com>
2019-03-13 12:05:17 +0100

    added changed properties to AWithId

--- Cranium.WPF/Helpers/AWithId.cs
+++ Cranium.WPF/Helpers/AWithId.cs
@@ -1,12 +1,34 @@
 ﻿using MongoDB.Bson;
 using MongoDB.Bson.Serialization.Attributes;
+using Newtonsoft.Json;
 using Prism.Mvvm;
+using System.Collections.ObjectModel;
+using System.Collections.Specialized;
+using System.ComponentModel;
+using System.Linq;
 
 namespace Cranium.WPF.Helpers
 {
     public abstract class AWithId : BindableBase, IWithId
     {
+        #region FIELDS
+
         private ObjectId _id;
+
+        #endregion FIELDS
+
+
+        #region CONSTRUCTOR
+
+        public AWithId()
+        {
+            PropertyChanged += OnPropertyChanged;
+        }
+
+        #endregion CONSTRUCTOR
+
+
+        #region PROPERTIES
 
         [BsonId]
         public ObjectId Id
@@ -14,5 +36,31 @@ namespace Cranium.WPF.Helpers
             get => _id;
             set => SetProperty(ref _id, value);
         }
+
+        [JsonIgnore]
+        [BsonIgnore]
+        public ObservableCollection<string> ChangedProperties { get; } = new ObservableCollection<string>();
+
+        #endregion PROPERTIES
+
+
+        #region METHODS
+
+        private void OnPropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+            if (ChangedProperties.Any(x => x == e.PropertyName))
+                return;
+
+            ChangedProperties.Add(e.PropertyName);
+
+            var property = GetType().GetProperty(e.PropertyName);
+            if (typeof(INotifyCollectionChanged).IsAssignableFrom(property.PropertyType))
+            {
+                var collectionChanged = (INotifyCollectionChanged)property.GetValue(this);
+                collectionChanged.CollectionChanged += (s, carg) => OnPropertyChanged(this, e);
+            }
+        }
+
+        #endregion METHODS
     }
 }
