f18b9b6676d91bf530ce715fbf0231b3e5b5a4af
wim van laer <van.laer.wim@gmail.com>
2019-03-18 12:39:56 +0100

    added model service interfae

---
+++ Cranium.Data/Services/IModelService.cs
@@ -0,0 +1,21 @@
+﻿using Shared;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Cranium.Data.Services
+{
+    public interface IModelService<T>
+       where T : IWithId
+    {
+        Task<T> CreateAsync(T item);
+
+        Task<T> GetOneAsync(Guid id);
+
+        Task<IList<T>> GetAsync();
+
+        Task UpdateAsync(T newItem);
+
+        Task RemoveAsync(Guid id);
+    }
+}
